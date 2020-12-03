ace68b169dcd1f96c9379b0ac4bba14b9e90dba2
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-03 08:32:20 +0200

    Refactor and solve part 2

--- day3/aoc2020d3.nim
+++ day3/aoc2020d3.nim
@@ -1,31 +1,36 @@
-# part 1 took 5 to 10 minutes
+# AoC 2020 day 2 solution
+# copyright 2020 by Charl P. Botha <info@charlbotha.com>
+# BSD 3-clause thanks
+
+# as usually the case, could only start at some time after 8 (UTC+2)
+# part 1 took 5 to 10 minutes. answer 203
+# part 2 start at 8:26 finish at 8:28
 
 import os, sequtils, strscans, strutils, tables
 
 # each line is a string of . (no trees) and # (trees)
 let lines = readFile(joinPath(getAppDir(), "input.txt")).strip().splitLines()
-
 let lineLen = lines[0].len
-# start at line 0, pos 0
-var row = 0
-var col = 0
-var atBottom = false
-var numTrees = 0
 
+proc countTrees(dx, dy: int): int =
+  # start at line 0, pos 0
+  var row = 0
+  var col = 0
+  var numTrees = 0
 
-while not atBottom:
-  if lines[row][col] == '#':
-    numTrees += 1
-  
-  col += 3
-  row += 1
+  while true:
+    if lines[row][col] == '#':
+      numTrees += 1
+    
+    col += dx
+    row += dy
 
-  if row >= lines.len:
-    break
+    if row >= lines.len:
+      return numTrees
 
-  # wrap around simulates the repetition of the rows
-  if col >= lineLen:
-    col = col - lineLen
+    # wrap around simulates the repetition of the rows
+    if col >= lineLen:
+      col = col - lineLen
 
-echo numTrees
+echo countTrees(1,1) * countTrees(3,1) * countTrees(5,1) * countTrees(7,1) * countTrees(1,2)
 
