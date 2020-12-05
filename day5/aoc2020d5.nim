3edd404d8ade8943d84060a9fe10e7d5f9ec9b1b
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-05 11:34:05 +0200

    Add working max seatId solution
    This binary encodes the row and col spec.

--- day5/aoc2020d5.nim
+++ day5/aoc2020d5.nim
@@ -1,4 +1,4 @@
-# AoC 2020 day 2 solution
+# AoC 2020 day 5 solution
 # copyright 2020 by Charl P. Botha <info@charlbotha.com>
 # BSD 3-clause thanks
 
@@ -12,6 +12,38 @@
 # = 44
 # haha nice
 
-import os, sequtils, strscans, strutils, tables
+import os, sequtils, strformat, strscans, strutils, tables
 
 let lines = readFile(joinPath(getAppDir(), "input.txt")).strip().splitLines()
+
+proc convertHalfSpecToNumber(halfSpec: string, zeroString: string, oneString: string): int =
+  let binStr = halfSpec.replace(zeroString, "0").replace(oneString, "1")
+  var num: int
+  if not scanf(binStr, "$b", num):
+    raise newException(ValueError, &"could not parse {binStr}")
+
+  result = num
+
+assert convertHalfSpecToNumber("FBFBBFF", "F", "B") == 44
+
+proc calcSeatId(seatSpec: string): int =
+  let rowPart = seatSpec[0..6]
+  let row = convertHalfSpecToNumber(rowPart, "F","B")
+
+  let colPart = seatSpec[7..9]
+  let col = convertHalfSpecToNumber(colPart, "L","R")
+
+  result = row * 8 + col
+
+assert calcSeatId("FBFBBFFRLR") == 357
+
+var maxId = -1
+for line in lines:
+  let seatId = calcSeatId(line)
+  if seatId > maxId:
+    maxId = seatId
+
+echo maxId
+
+
+
