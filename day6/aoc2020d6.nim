7a3ab3226b915f2ba2f34563305f8d23fde68fd6
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-06 15:20:42 +0200

    Add notes and simpler \n\n split

--- day6/aoc2020d6.nim
+++ day6/aoc2020d6.nim
@@ -2,17 +2,26 @@
 # copyright 2020 by Charl P. Botha <info@charlbotha.com>
 # BSD 3-clause thanks
 
-import os, re, sequtils, sets, strformat, strscans, strutils, tables
+# New nim-tricks I leanned today:
+# - re"^$" is short for re("^$), but you need second form to add set of regex options
 
-# this time simply munging directly into groups
-# instead of doing per-line grouping as with day 4
-let reEmptyLine = re("^$", {reMultiLine})
-let lines = readFile(joinPath(getAppDir(), "input.txt")).strip().split(reEmptyLine)
+import os, sets, strutils, tables
+
+# this time I'm simply splitting the input directly into groups
+# instead of doing per-line parsing and grouping as with day 4.
+# per-line would of course be the better real-world solution because
+# can handle super large input files
+
+# yes, you can split on a regex for the empty line
+# let reEmptyLine = re("^$", {reMultiLine})
+# let lines = readFile(joinPath(getAppDir(), "input.txt")).strip().split(reEmptyLine)
+# but splitting on \n\n is cleverer. thanks sjvdwalt!
+let lines = readFile(joinPath(getAppDir(), "input.txt")).strip().split("\n\n")
 
 var totalYesCount = 0
 var totalAllYesCount = 0
 for group in lines:
-  # "group" is now e.g.: nsi\nvlsgi\nins\nsi\n (i.e. ater we split file on empty lines)
+  # "group" is now e.g.: nsi\nvlsgi\nins\nsi\n (i.e. after we split file on empty lines)
   let persons = group.strip().split()
   # a group of four persons now looks like e.g. @["nsi", "vlsig", "ins", "si"]
   # we join to create one string, and then simply count unique letters
@@ -26,5 +35,5 @@ for group in lines:
     if count[k] == numPersons:
       totalAllYesCount += 1
 
-echo totalYesCount
-echo totalAllYesCount
+assert totalYesCount == 7128
+assert totalAllYesCount == 3640
