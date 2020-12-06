d7869b725757f2b1497e5e18a877248d56ef3504
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-06 08:25:22 +0200

    Make working part 2 solution

--- day6/aoc2020d6.nim
+++ day6/aoc2020d6.nim
@@ -10,12 +10,21 @@ let reEmptyLine = re("^$", {reMultiLine})
 let lines = readFile(joinPath(getAppDir(), "input.txt")).strip().split(reEmptyLine)
 
 var totalYesCount = 0
+var totalAllYesCount = 0
 for group in lines:
-  # a group of four people looks like e.g. @["nsi", "vlsig", "ins", "si"]
+  # "group" is now e.g.: nsi\nvlsgi\nins\nsi\n (i.e. ater we split file on empty lines)
+  let persons = group.strip().split()
+  # a group of four persons now looks like e.g. @["nsi", "vlsig", "ins", "si"]
   # we join to create one string, and then simply count unique letters
   # answering question: to which questions did at least one person in group answer yes
-  totalYesCount += group.strip().split().join("").toHashSet().len
+  totalYesCount += persons.join("").toHashSet().len
 
   # part 2: to which questions did ALL persons in group answer yes?
+  let numPersons = persons.len
+  let count = persons.join("").toCountTable
+  for k in count.keys():
+    if count[k] == numPersons:
+      totalAllYesCount += 1
 
 echo totalYesCount
+echo totalAllYesCount
