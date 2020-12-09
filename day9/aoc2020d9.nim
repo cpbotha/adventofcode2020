bc69ceb755cc0c11bbe4352f23488675af806139
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-09 08:32:52 +0200

    Add cleaned-up day 9 part 2 solution

--- day9/aoc2020d9.nim
+++ day9/aoc2020d9.nim
@@ -7,19 +7,41 @@ var nums = readFile(joinPath(getAppDir(), "input.txt")).strip().split("\n").mapI
 
 proc areThereNumsThatAddUpTo(idx: int): bool =
   result = false
-  echo idx
   # outer loop skips last pos, because inner loop will go there
   for i in idx-windowLen..idx-2:
     for j in i+1..idx-1:
       if nums[i] + nums[j] == nums[idx]:
         return true
 
-for idx in preambleLen..nums.len-1:
-  let num = nums[idx]
-  # now find two numbers in the windowLen behind us that add up to num
-  if not areThereNumsThatAddUpTo(idx):
-    # my answer is 32321523
-    echo "====> ", num
-    break
+proc findFirstNumThatIsNotSum(): int =
+  for idx in preambleLen..nums.len-1:
+    let num = nums[idx]
+    # now find two numbers in the windowLen behind us that add up to num
+    if not areThereNumsThatAddUpTo(idx):
+      # my answer is 32321523
+      return num
 
-  
+  raise newException(ValueError, "No number that is the sum of two nums in preceding window")
+
+let part1num = findFirstNumThatIsNotSum()
+assert part1num == 32321523
+
+
+# part 2 ==================
+
+proc findMinMaxOfContigSum(): int =
+  result = -1
+  for idx in 0..nums.len-1:
+    var sum = 0
+    for i in idx..nums.len-1:
+      sum += nums[i]
+      if sum > part1num:
+        # already too big, break out of the inner for loop
+        break
+
+      if sum == part1num:
+        # this means everything from idx..i
+        let contig = nums[idx..i]
+        return contig.min() + contig.max()
+
+assert findMinMaxOfContigSum() == 4794981
