1c68e6692df662d9b3754f80c63e1e2fa9327c5d
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-10 12:55:53 +0200

    Add translation of K Carruthers-Smith's DP soln

--- day10/aoc2020d10.nim
+++ day10/aoc2020d10.nim
@@ -2,7 +2,9 @@
 # copyright 2020 by Charl P. Botha <info@charlbotha.com>
 # BSD 3-clause thanks
 
-# start part 1 at 08:04, completed at around 08:25
+# - start part 1 at 08:04, completed at around 08:25
+# - part 2: I could not get my backtracking to work on the full data. Gave up,
+#   started looking at other people's solutions.
 
 # p1
 # - input file has rated output joltages of adapters
@@ -27,6 +29,13 @@
 # - when done building the graph, count the nodes
 # start at level 0, the whole set. level 1 is a seq of hashsets, where each hashset contains only the removed adapter idx
 # level 2 is a seq of hashsets, but here the hashsets have two elements, and so on.
+
+# part 2 solutions from reddit https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/
+# - dynamic programming, which is super fast, and is quite generic
+# - peeps recognizing that sequences of joltage difference 1 result in specific
+#   numbers of permutations for that sub-sequence
+# - tribonacci
+
 
 import algorithm, os, sequtils, sets, strformat, strutils, tables
 
@@ -58,10 +67,11 @@ proc part1(): int =
 
 #assert part1() == 2450
 
-var cache = initTable[HashSet[int], bool]()
+# cache takes a lot of memory
+#var cache = initTable[HashSet[int], bool]()
 proc checkValidSequence(excludedIndices: HashSet[int]): bool =
-  if excludedIndices in cache:
-    return cache[excludedIndices]
+  #if excludedIndices in cache:
+  #  return cache[excludedIndices]
 
   var curVoltage = 0
   for idx, a in adapterOutputs:
@@ -74,13 +84,17 @@ proc checkValidSequence(excludedIndices: HashSet[int]): bool =
       # invalid break in chain, early out
       return false
 
+  # ensure that the last jump is valid
   result = curVoltage + 3 == deviceJoltage
-  cache[excludedIndices] = result
+  #cache[excludedIndices] = result
 
 # we start with a working sequence, we know this from part 1
 var numSolutions = 1
 
-proc walkTreeBT(excluded: HashSet[int] = initHashSet[int]()): int =
+# quite bummed man
+# this backtracking(ish) solution works for both the demo datasets,
+# but doesn't get very far on my actual input after some minutes
+proc walkTreeBT(excluded: HashSet[int] = initHashSet[int]()): void =
   var start = 0
   if excluded.len > 0:
     start = excluded.toSeq().max()
@@ -93,18 +107,43 @@ proc walkTreeBT(excluded: HashSet[int] = initHashSet[int]()): int =
       # already excluded, so try and find another adapter to exclude
       continue
 
-    # remove the candidate adapter
+    # exclude this candidate adapter
     let newBranch = excluded + toHashSet(@[idx])
 
     if newBranch.len > 0 and newBranch.len <= 3: echo "exploring ", newBranch
 
+    # then check if we should count this solution as valid, and continue down the tree to extend it
     if checkValidSequence(newBranch):
       # valid solution
       numSolutions += 1
       #echo "total ", numSolutions, " solutions with ", newBranch
       # we need to continue down there
-      discard walkTreeBT(newBranch)
+      walkTreeBT(newBranch)
 
-  return numSolutions
+# I could not get my backtracking algorithm above to work, and so turned to zatech:
+# solveB below is a translation + commenting of Keegan Carruthers-Smith's DP solution in Python
+# https://github.com/keegancsmith/advent/blob/master/2020/10/10.py
+proc solveB(adapters: seq[int]): int =
+   var dp = @[1]
+   for i in 1..adapters.len-1:
+       var acc = 0
+       let x = adapters[i]
 
-echo walkTreeBT()
+       # for the three adapters up to just before x, if diff is <= 3
+       # it means that adapter can connect to x
+       # (no point in going further back, because max diff is 3V)
+       # for each of the previous solutions dp[j] this is a valid new solution, so accumulate
+       for j in max(0, i-3)..i-1:
+           if x - adapters[j] <= 3:
+               acc += dp[j]
+       
+       # when done accumulating, record for the next step
+       dp.add(acc)
+
+   return dp[^1]
+
+
+#walkTreeBT()
+#echo numSolutions
+
+echo solveB(@[0] & adapterOutputs & @[deviceJoltage])
