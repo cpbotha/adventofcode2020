f302235b257de9819c0cffd3866c0a9daf1552de
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-14 09:49:54 +0200

    Add working part 2 solution

--- day14/aoc2020d14.nim
+++ day14/aoc2020d14.nim
@@ -34,4 +34,60 @@ proc doPart1(): int =
 
   result = toSeq(mem.values).foldl(a+b)
 
-echo doPart1()
+assert doPart1() == 17028179706934
+
+# 0 -> addrress bit unchanged
+# 1 -> address bit overwritten with 1
+# X - > address bit floating
+proc doPart2(): int =
+  var
+    mem = newTable[int,int]()
+    curMaskStr: string
+    curAndMask: int
+    curOrMask: int
+    floatOffsets = newSeq[int]()
+    dummy: string
+    ad: int
+    adStr: string
+    val: int
+  
+  for li, l in instructions:
+    if l.startsWith("mask"):
+      curMaskStr = align(l["mask = ".len..^1], 36, padding='0')
+      # curOrMask to switch on bits
+      let so = cast[string](curMaskStr.mapIt(if it == '1': '1' else: '0'))
+      curOrMask = parseBinInt(so)
+      # now calculate the offsets that are floating
+      floatOffsets = @[]
+      for i,c in curMaskStr:
+        if c == 'X':
+          # offsets from the left
+          floatOffsets.add(i)
+
+    else:
+      # mem[8] = 11
+      if scanf(l, "$w[$i] = $i", dummy, ad, val):
+        # switch on the 1 bits
+        ad = bitor(ad, curOrMask)
+        adStr = &"{ad:036b}"
+
+        # now iterate through all of the float combos
+        for i in 0..int(pow(2.0,float(floatOffsets.len)))-1:
+          # convert current permutation to binary string in fstr
+          #let fstr1 = &" what why???! {i}:b" <--- doh.
+          var fstr: string
+          formatValue(fstr, i, "b")
+          fstr = align(fstr, floatOffsets.len, padding='0')
+
+          for bidx, fb in fstr:
+            adStr[floatOffsets[bidx]] = fb
+
+          ad = parseBinInt(adStr)
+          # now store at this modified address
+          #echo floatOffsets
+          #echo "addr: ", ad, " ", adStr, " maskstr ", curMaskStr
+          mem[ad] = val
+
+  result = toSeq(mem.values).foldl(a+b)
+
+assert doPart2() == 3683236147222
