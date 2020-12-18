2865b91b5243bae99f62f2e2659fb0c6b83eeeff
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-18 09:45:23 +0200

    Add equation rewriting with parens for precedence

--- day18/aoc2020d18.nim
+++ day18/aoc2020d18.nim
@@ -5,6 +5,12 @@
 # simple acc + op logic
 
 # part 2 ======
+# we need an https://en.wikipedia.org/wiki/Operator-precedence_parser
+# ex1: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+# ex2: Pratt Parsing
+# ex3: precedence climbing method
+# ex4: rewrite equation with parentheses, see https://en.wikipedia.org/wiki/Operator-precedence_parser#Alternative_methods
+# I ended up getting ex4 to work by swapping rewrite rule for * and +. :D
 
 import os, re, sets, sequtils, strformat, strutils
 
@@ -54,13 +60,37 @@ proc evalEqn(eqn: string, startidx: int = 0): (int, int) =
       break
 
   result = (acc, i)
-
     
+# adapted from the C code at https://en.wikipedia.org/wiki/Operator-precedence_parser#Alternative_methods
+proc convertEqnWithParens(eqn: string): string =
+  result = "(((("
+  for i,c in eqn:
+    case c
+    of '(':
+      result &= "(((("
+      continue
+    of ')':
+      result &= "))))"
+      continue
+    of '+':
+      # we rewrite + in the same way the published algo rewrites *, because
+      # we want it to take precedence
+      result &= ")) + (("
+      continue
+    of '*':
+      result &= "))) * ((("
+      continue
+    else:
+      result &= c
 
-
-    
+  result &= "))))"
 
 proc doPart1(): int =
   eqns.mapIt(evalEqn(it)[0]).foldl(a+b)
 
-echo doPart1()
+echo doPart1() == 45840336521334
+
+proc doPart2(): int =
+  eqns.mapIt( evalEqn(convertEqnWithParens(it))[0] ).foldl(a+b)
+
+assert doPart2() == 328920644404583
