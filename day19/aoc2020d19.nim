49b640b221aac885271b9dc2ea87a2b7eba7076a
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-19 12:59:33 +0200

    Add solution for day 19 part 2

--- day19/aoc2020d19.nim
+++ day19/aoc2020d19.nim
@@ -1,9 +1,17 @@
+# AoC 2020 day 19 solution
+# copyright 2020 by Charl P. Botha <info@charlbotha.com>
+# BSD 3-clause thanks
+
 # part 1 plan ====
-# - we want LUT with rulenum: regexp including |
-# - 
-# 
+# - we want LUT with rulenum: regexp including
+# - ok, at least I had the idea early on to pack rules into regexps
+# - in spite of that, only had suitable data representation at third try (went to the beach in between)
+#   1. seq of seqs nope
+#   2. packing rulenums as ASCII chars into strings nope
+#   3. map string to seq[string] in input Dict, string to regexp string in final dict yeah
 
-
+# part 2 plan =====
+# - whimper
 
 import os, re, sequtils, sets, strformat, strscans, strutils, tables
 
@@ -12,17 +20,11 @@ import os, re, sequtils, sets, strformat, strscans, strutils, tables
 let sections = readFile(joinPath(getAppDir(), "input.txt")).strip().split("\n\n")
 
 # this is the desired ruledict
-var reTable = initTable[char, string]()
-
-# 108
-const OR_CHAR = 'l'
-
-# convert string rule number into its character encoding
-proc encodeRuleNum(numStr: string): char =
-  chr(ord(OR_CHAR) + parseInt(numStr))
+# map from string rule number to ready-to-go regexp
+var reTable = initTable[string, string]()
 
 # read input rules and return, also init reTable with the two known rules
-proc extractInputRules(): Table[char, string] =
+proc extractInputRules(): Table[string, seq[string]] =
   var matches: array[5, string]
   let rdone = re("(\\d+): \"([a-b])\"")
   let rpipe = re("(\\d+): (\\d+) (\\d+) | (\\d+) (\\d+)")
@@ -32,32 +34,55 @@ proc extractInputRules(): Table[char, string] =
     if '"' in ir:
       if match(ir, rdone, matches):
         # this goes straight into the reTable
-        reTable[encodeRuleNum(matches[0])] = matches[1]
+        reTable[matches[0]] = matches[1]
       else:
         raise newException(ValueError, &"could not parse {ir}")
 
     else:
       let numval = ir.split(": ")
-      # pack the whole rule value into a string
-      let valstr = numval[1].strip().split(" ").mapIt(if it == "|": OR_CHAR else: encodeRuleNum(it)).join()
-      result[encodeRuleNum(numval[0])] = valstr
+      # 
+      let valseq = numval[1].strip().split(" ")
+      result[numval[0]] = valseq
       
-
+proc maybeParens(term: string): string =
+  if term.contains("|"):
+    &"({term})"
+  else:
+    term
 
 proc doPart1(): int =
-  let inputRules = extractInputRules()
+  var inputRules = extractInputRules()
   # scan repeatedly through input rules
+  # until rule 0 is finalised with substitutions
+  while "0" in inputRules:
+    var toDelete = newSeq[string]()
+    for ir_num, ir_seq in inputRules:
 
-  assert ord(OR_CHAR) + len(inputRules) < 255
+      # replace all tokens in input rule with final values that might exist in reTable
+      # this means that a token in newSeq can be a string of characters starting with a or b
+      let newSeq = ir_seq.mapIt(reTable.getOrDefault(it, it))
 
-  for ir_num, ir_val in inputRules:
-    # go through ir_val, replacing each known rulenum
-    for c in ir_val:
-      if c in reTable:
-        ir_val.replace(&"{c}", reTable[c])
+      if newSeq != ir_seq:
+        # this means we have replaced stuff!
+        # check if new rule is complete: only a, b, |
+        if newSeq.allIt(it.startsWith("(") or it.startsWith("a") or it.startsWith("b") or it == "|"):
+          # store in final reTable as a complete string
+          reTable[ir_num] = maybeParens(newSeq.join())
+          # schedule removal from inputRules
+          toDelete.add(ir_num)
+          
+        else:
+          # replaced previous rule with transformed rule
+          inputRules[ir_num] = newSeq
 
+    #now remove the rules that have graduated to reTable
+    for ir_num in toDelete:
+      inputRules.del(ir_num)
 
-
+  # now check the messages
+  # make sure we match the whole stringa
+  let zre = re("^" & reTable["0"] & "$")
+  result = sections[1].strip().splitLines().filterIt(it.match(zre)).len
 
 echo doPart1()
 
