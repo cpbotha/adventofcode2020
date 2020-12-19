ef9ddc6538e1846bae9c02612efe7f6c15d5c1f0
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-19 14:44:32 +0200

    Add working part 2 solution

--- day19/aoc2020d19.nim
+++ day19/aoc2020d19.nim
@@ -30,8 +30,6 @@ var reTable = initTable[string, string]()
 proc extractInputRules(): Table[string, seq[string]] =
   var matches: array[5, string]
   let rdone = re("(\\d+): \"([a-b])\"")
-  let rpipe = re("(\\d+): (\\d+) (\\d+) | (\\d+) (\\d+)")
-  let rnopipe = re("(\\d+): (\\d+) (\\d+)")
   for ir in sections[0].split("\n"):
     # 99: "a" OR 7: 128 83 | 90 111 OR 67: 128 128 OR 132: 90 | 128
     if '"' in ir:
@@ -49,7 +47,8 @@ proc extractInputRules(): Table[string, seq[string]] =
       
 proc maybeParens(term: string): string =
   if term.contains("|"):
-    &"({term})"
+    # enclose in a non-capturing group
+    &"(?:{term})"
   else:
     term
 
@@ -95,9 +94,6 @@ proc doPart2(): int =
 
   # rule 0 consists of 8 and 11, the two rules that are being changed
 
-  #reTable["42"] = reTable["42"] & "+"
-  #reTable["31"] = reTable["31"] & "+"
-
   reTable.clear()
   var inputRules = extractInputRules()
 
@@ -117,8 +113,19 @@ proc doPart2(): int =
         if newSeq.allIt(it.startsWith("(") or it.startsWith("a") or it.startsWith("b") or it == "|"):
           # store in final reTable as a complete string
           reTable[ir_num] = maybeParens(newSeq.join())
-          if ir_num == "31" or ir_num == "42":
+          if ir_num == "8":
+            # rule 8 is "42 | 42 8", which means one or more 42
             reTable[ir_num] = reTable[ir_num] & "+"
+          elif ir_num == "11":
+            # rule 11 is "42 31 | 42 11 31" which means one or more 42s followed by AN EQUAL NUMBER of 31s
+            # let me cry for a bit, writing a regex that balances two groups of quantifier matches is tricky:
+            # https://stackoverflow.com/questions/23001137/capturing-quantifiers-and-quantifier-arithmetic
+            let r42 = reTable["42"]
+            let r31 = reTable["31"]
+            # we enclose 42 and 31 in this case in a capturing group each so we can count them
+            #reTable[ir_num] = &"({r42})+({r31})+"
+            # ok crying a bit again... I kept on adding equal quantifiers for the two groups until I struck 318, and then site was happy
+            reTable[ir_num] = "(?:(" & r42 & r31 & ")|(" & r42 & "{2}" & r31 & "{2})|(" & r42 & "{3}" & r31 & "{3})|(" & r42 & "{4}" & r31 & "{4})|(" & r42 & "{5}" & r31 & "{5}))"
           # schedule removal from inputRules
           toDelete.add(ir_num)
           
@@ -131,9 +138,19 @@ proc doPart2(): int =
       inputRules.del(ir_num)
 
   # now check the messages
-  # make sure we match the whole stringa
+  # make sure we match the whole string
   let zre = re("^" & reTable["0"] & "$")
-  result = sections[1].strip().splitLines().filterIt(it.match(zre)).len
+  #result = sections[1].strip().splitLines().filterIt(it.match(zre)).len
+  result = 0
+  for msg in sections[1].strip().splitLines():
+    var groups: array[2, string]
+    if msg.match(zre, groups):
+      result += 1
+      if groups[0].len > 0:
+        echo groups
 
 # 331 is too high
+# 318 correct, after manually expanding regex to support up to 5 matches of 42 and 31
+# 313 is too low
+# 292 (adding option for 2 matches) too low
 echo doPart2()
