43c4433f7d6b895a4743d63205e60774a93f1d59
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-19 12:18:13 +0200

    Aborted attempt #2: string packing

--- day19/aoc2020d19.nim
+++ day19/aoc2020d19.nim
@@ -12,9 +12,17 @@ import os, re, sequtils, sets, strformat, strscans, strutils, tables
 let sections = readFile(joinPath(getAppDir(), "input.txt")).strip().split("\n\n")
 
 # this is the desired ruledict
-var reTable = initTable[string, string]()
+var reTable = initTable[char, string]()
 
-proc extractInputRules(): Table[string, seq[seq[string]]] =
+# 108
+const OR_CHAR = 'l'
+
+# convert string rule number into its character encoding
+proc encodeRuleNum(numStr: string): char =
+  chr(ord(OR_CHAR) + parseInt(numStr))
+
+# read input rules and return, also init reTable with the two known rules
+proc extractInputRules(): Table[char, string] =
   var matches: array[5, string]
   let rdone = re("(\\d+): \"([a-b])\"")
   let rpipe = re("(\\d+): (\\d+) (\\d+) | (\\d+) (\\d+)")
@@ -24,28 +32,35 @@ proc extractInputRules(): Table[string, seq[seq[string]]] =
     if '"' in ir:
       if match(ir, rdone, matches):
         # this goes straight into the reTable
-        reTable[matches[0]] = matches[1]
+        reTable[encodeRuleNum(matches[0])] = matches[1]
       else:
         raise newException(ValueError, &"could not parse {ir}")
 
     else:
       let numval = ir.split(": ")
-      # ruleval could contain |
-      let ruleterms = numval[1].split("|")
-      # each rule term should end up in seq[str]; it can contain arb number of nums
-      result[numval[0]] = ruleterms.mapIt(it.strip().split(" ").mapIt(it.strip()))
+      # pack the whole rule value into a string
+      let valstr = numval[1].strip().split(" ").mapIt(if it == "|": OR_CHAR else: encodeRuleNum(it)).join()
+      result[encodeRuleNum(numval[0])] = valstr
+      
+
 
 proc doPart1(): int =
   let inputRules = extractInputRules()
   # scan repeatedly through input rules
 
-  for num, rule in inputRules:
-    var ruleDone = false
-    for orterm in rule:
-      for term in orterm:
-        if term in reTable:
-          echo "yargh, this is not going to work because have to add new orterms all the time!"
+  assert ord(OR_CHAR) + len(inputRules) < 255
+
+  for ir_num, ir_val in inputRules:
+    # go through ir_val, replacing each known rulenum
+    for c in ir_val:
+      if c in reTable:
+        ir_val.replace(&"{c}", reTable[c])
+
 
 
 
 echo doPart1()
+
+#echo ord('b'), " ", ord('l')
+
+#echo "90 90 | 132 128".split(" ").mapIt(if it == "|": '/' else: 'a').join()
