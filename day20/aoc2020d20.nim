d48ff519de664df1cc3a17cc3fd29e10966f85f1
Charl P. Botha <cpbotha@vxlabs.com>
2020-12-20 15:51:23 +0200

    Add more comments

--- day20/aoc2020d20.nim
+++ day20/aoc2020d20.nim
@@ -1,3 +1,10 @@
+# AoC 2020 day 20 non-working attempt
+# copyright 2020 by Charl P. Botha <info@charlbotha.com>
+# BSD 3-clause thanks
+
+# Sunday, day 20 this is where I dropped out :) (or at the very least gave up on this puzzle)
+# this attempt contains some of my thinking until I decided enough
+
 # part 1 plan =====
 # - edge-matching puzzle with flips
 # - encode each edge as binary number
@@ -10,6 +17,10 @@
 # - after a first check, it turns out that unflipped edges occur at most twice
 #   iow not as hard as it appeared at first sight
 
+# later:
+# it turns out the problem was indeed very tightly constrained so you just had to slog through it placing tiles
+# after having found the four corner tiles (tiles with two adjacent unique edges, unique even when flipped)
+
 import algorithm, bitOps, os, parseUtils, re, sequtils, sets, strformat, strutils, tables
 
 # 144 tile = 12*12
@@ -19,6 +30,13 @@ let tiles = readFile(joinPath(getAppDir(), "input.txt")).strip().split("\n\n")
 proc edgeToNum(edge: string): int =
   # # = 1, . = 0
   assert parseBin(edge.replace("#", "1").replace(".", "0"), result) == 10
+
+# if edge is low number, reverseBits would give wrong answer
+# we have to pad string repr to 10 bits, then reverse, then back to int
+# BTW I love the nim ergonomics here of supporting return/result-less function returns
+# feels a bit like f# in that sense also
+proc flipIntEdge(edge: int): int =
+  parseBinInt(reversed(&"{edge:010b}").join())  
 
 var edgeToTile = initTable[int, HashSet[string]]()
 var tileToEdges = initTable[string, array[4,int]]()
@@ -72,9 +90,16 @@ proc extendSolution(tileLayout: TTileLayout, placedTiles: TPlacedTiles): (TTileL
   var
     leftEdge = 0
     topEdge = 0
+    candidates: initHashSet[string]()
+
   if col - 1 >= 0:
     # this means we have a left edge
     leftEdge = tileLayout[row][col-1].right
+    if leftEdge in edgeToTile:
+      candidates.incl(edgeToTile[leftEdge])
+    
+    # now flip it
+
   if row - 1 >= 0:
     # this means we have a top edge
     topEdge = tileLayout[row-1][col].bottom
@@ -93,9 +118,7 @@ proc doPart1(): int =
   # root of the BT tree branches out into 144 * 4 rotations * 2 flips per rotation (not flipped, flipped)
   for tile, edges in tileToEdges:
     for ei, edge in edges:
-      # if edge is low number, reverseBits would give wrong answer
-      # we have to pad to 10 bits, then reverse
-      for fEdge in [edge, parseBinInt(reversed(&"{edge:010b}").join())]:
+      for fEdge in [edge, flipIntEdge(edge)]:
         var bottomEdge: int
         if fEdge == edge:
           bottomEdge = edges[if ei + 1 < edges.high(): ei+1 else: 0]
